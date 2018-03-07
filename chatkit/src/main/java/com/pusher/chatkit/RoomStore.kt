e2bae612b00d236b8ea3f5cefd4e7040338c22a4
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-03-07 16:21:29 +0000

    Added some formatting

--- chatkit/src/main/java/com/pusher/chatkit/RoomStore.kt
+++ chatkit/src/main/java/com/pusher/chatkit/RoomStore.kt
@@ -4,13 +4,13 @@ import java.util.concurrent.ConcurrentMap
 
 class RoomStore(val roomsMap: ConcurrentMap<Int, Room>) {
 
-    val rooms : List<Room>
+    val rooms: List<Room>
         get() = roomsMap.values.toList()
 
     fun addOrMerge(room: Room) {
-        if (roomsMap[room.id] != null){
+        if (roomsMap[room.id] != null) {
             roomsMap[room.id]!!.updateWithPropertiesOfRoom(room)
-        } else{
+        } else {
             roomsMap[room.id] = room
         }
     }
