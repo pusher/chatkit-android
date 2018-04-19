fe44cba93ead486f6cfe423c35f41aae06618554
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-19 12:09:48 +0100

    Add ChatManager spek

---
+++ chatkit-core/src/integrationTest/kotlin/com/pusher/chatkit/ChatManagerSpek.kt
@@ -0,0 +1,42 @@
+package com.pusher.chatkit
+
+import com.google.common.truth.Truth
+import com.pusher.chatkit.test.FutureValue
+import com.pusher.chatkit.test.Timeout
+import com.pusher.chatkit.test.will
+import elements.Subscription
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import elements.Error as ElementsError
+
+private val INSTANCE_LOCATOR: String = System.getProperty("chatkit_integration_locator") ?: "Missing gradle/system property 'chatkit_integration_locator'"
+private val INSTANCE_ID = INSTANCE_LOCATOR.split(":").getOrNull(2) ?: "Missing instance id in locator (property 'chatkit_integration_locator')"
+private val USER_NAME: String = System.getProperty("chatkit_integration_username") ?: "Missing gradle/system property 'chatkit_integration_username'"
+
+class ChatManagerSpek : Spek({
+
+    describe("ChatManager with valid instance") {
+
+        val manager = ChatManager(
+            instanceLocator = INSTANCE_LOCATOR,
+            userId = USER_NAME,
+            dependencies = TestChatkitDependencies(
+                tokenProvider = TestTokenProvider(INSTANCE_ID, USER_NAME)
+            )
+        )
+
+        will("load current user", Timeout.None) {
+            var sub by FutureValue<Subscription>()
+            sub = manager.connect { event ->
+                done {
+                    val currentUser = event.let { it as? CurrentUserReceived }?.currentUser
+                    Truth.assertThat(currentUser?.id).isEqualTo(USER_NAME)
+                    sub.unsubscribe()
+                }
+            }
+        }
+
+    }
+
+})
+
