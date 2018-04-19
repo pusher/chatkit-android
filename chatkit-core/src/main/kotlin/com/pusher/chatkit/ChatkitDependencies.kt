62273c2b448f2b6799f03a2af0b566cc9d0dc2e5
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-19 12:09:30 +0100

    Expose OkHttp on ChatManager

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitDependencies.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitDependencies.kt
@@ -2,10 +2,12 @@ package com.pusher.chatkit
 
 import com.pusher.platform.PlatformDependencies
 import com.pusher.platform.tokenProvider.TokenProvider
+import okhttp3.OkHttpClient
 
 interface ChatkitDependencies : PlatformDependencies {
 
     val tokenProvider: TokenProvider
     val tokenParams: ChatkitTokenParams?
+    val okHttpClient : OkHttpClient?
 
 }
