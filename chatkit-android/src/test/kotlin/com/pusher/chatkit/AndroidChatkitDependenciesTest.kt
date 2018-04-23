af732b651841a4bd7e6c6c6ba4ca6c78aed0f2cc
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-04-23 16:30:23 +0100

    Add test for chatkit-android

---
+++ chatkit-android/src/test/kotlin/com/pusher/chatkit/AndroidChatkitDependenciesTest.kt
@@ -0,0 +1,25 @@
+package com.pusher.chatkit
+
+import com.google.common.truth.Truth.*
+import com.pusher.SdkInfo
+import mokitox.stub
+import org.junit.jupiter.api.Test
+
+internal class AndroidChatkitDependenciesTest {
+
+    @Test
+    fun `sdkInfo is injected`() {
+        val dependencies = AndroidChatkitDependencies(
+            context = stub(),
+            tokenProvider = stub()
+        )
+
+        assertThat(dependencies.sdkInfo).isEqualTo(SdkInfo(
+            product = "Chatkit",
+            sdkVersion = BuildConfig.VERSION_NAME,
+            platform = "Android",
+            language = "Kotlin/Java"
+        ))
+    }
+
+}
