952bee68bc93ac7004b3f33d8112ec5ba6b348b9
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-05-11 14:48:18 +0100

    Retrofitted fix to the supervisor

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/users/User.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/users/User.kt
@@ -2,20 +2,24 @@ package com.pusher.chatkit.users
 
 import com.pusher.chatkit.CustomData
 import com.pusher.chatkit.presence.Presence
+import com.pusher.chatkit.util.dateFormat
 
 data class User(
     val id: String,
     val createdAt: String,
-    var updatedAt: String,
+    val updatedAt: String,
 
-    var name: String?,
-    var avatarURL: String?,
-    var customData: CustomData?,
+    val name: String?,
+    val avatarURL: String?,
+    val customData: CustomData?,
     private var online: Boolean = false
 ) {
 
     var presence: Presence
         get() = if(online) Presence.Online else Presence.Offline
         set(value) { online = value === Presence.Online }
+
+    val created by lazy { dateFormat.parse(createdAt) }
+    val updated by lazy { dateFormat.parse(updatedAt) }
 
 }
