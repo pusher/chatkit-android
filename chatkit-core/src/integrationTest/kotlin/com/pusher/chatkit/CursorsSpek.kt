e3dbf1c4aadbb9c3d0cb8d32e5cbb606466f23ff
Pablo Gonzalez Alonso <pabs87@gmail.com>
2018-05-14 13:59:10 +0000

    Cursors test (#31)
    * Upgrade gradle and dependency version references
    
    * Split code into core/android modules
    
    * Updated for new split platform structure
    
    * Add supporting file for tests
    
    * Add jwt for auth
    
    * Expose OkHttp on ChatManager
    
    * Add ChatManager spek
    
    * Remove unlimited timeout
    
    * Add coverage reporting
    
    * Add system properties
    
    * Need to override not assign the properties (plus some doc)
    
    * TRavis uses env vars, not system ones
    
    * Add overridable timeout
    
    * Try to upgrade JVM
    
    * Small fix on injectable timeout
    
    * Extracted key for instance
    
    * Remove double colon
    
    * Small fixxes
    
    * Make the user the future value instead of the subscription
    
    * Removed unneded build
    
    * Update kotlin to 1.2.40
    
    * Run tests properly
    
    * Add android jacoco report
    
    * Add test for chatkit-android
    
    * Missing mockito dependency
    
    * Application context cannot be null
    
    * Combine codeCoverageReport run
    
    * Avoid double instrumentation
    
    * Avoid global codeCoverageReport
    
    * Add badges
    
    * Add supervisor for instance clearup
    
    * Crete instance supervisor
    
    * Add test for loading rooms
    
    * Add tests for known users
    
    * Simplified chatmanager spek
    
    * Receive message from alice
    
    * Add global closure
    
    * Simplified spek further
    
    * Partial disable of test
    
    * Let's try without alice
    
    * No room?
    
    * No alice manager
    
    * Closure may be the issue
    
    * Maybe is the room subscription that needs to be released
    
    * Enable coroutines
    
    * Let's try with local chat manager
    
    * change order of gradle tasks
    
    * Disable working tests
    
    * This makes no sense
    
    * Let's try without complext concurrent set up
    
    * remove timeout
    
    * typo after using default spek scopes
    
    * Retrofited supervisor logic from next ticket
    
    * mockito not used for now
    
    * No mockito on fixtures
    
    * Let's put mockito extensions back in
    
    * Introduced future to match promise on JS library
    
    * Removed room state machine
    
    * Add gradle combined build task for ci
    
    * ci combined build task
    
    * Update tests to use futures
    
    * Update code to match new platform sdk
    
    * updated to latest implementation of platform
    
    * Move ChatManagerEvents to their own file and added support for listeners and events
    
    * Add create room ro current user
    
    * Update code for latest from platform
    
    * Updated to be happy with latests platform version
    
    * Change dependency to platform 0.4.0
    
    * Rewriting the docs part 1
    
    * New lines
    
    * lists
    
    * Finished room sections
    
    * Add small note to room deletion
    
    * Fix compilation issue
    
    * subscriptions
    
    * Users
    
    * Finished up squeleton
    
    * First complete readme draft
    
    * Add support for cursrors and started clean up
    
    * Created test for start typing event (and removed stopped typing)
    
    * Restored presence
    
    * Add random name space to avoid test clash
    
    * Add coverage for room events
    
    * Add presence tests
    
    * first cursor test
    
    * disable room tests
    
    * remove cloning
    
    * disable first half of tests
    
    * disable only first
    
    * Disable second
    
    * Restore tests
    
    * add timeout to FutureValue
    
    * Small tweak to travis script
    
    * No add-on
    
    * made tests more semantic
    
    * cleaned up travis file
    
    * type in script
    
    * Checking if folder exists for cache
    
    * travis bash tweaks
    
    * bash is a bit alien like
    
    * syntax better?
    
    * always unzip
    
    * add semantics to test names
    
    * more semantic test names
    
    * upgrade gradle
    
    * Add token provider and result docs
    
    * reverted changes from merge
    
    * reverted changes from merge
    
    * reverted changes from merge
    
    * add wait for instance to be idle

---
+++ chatkit-core/src/integrationTest/kotlin/com/pusher/chatkit/CursorsSpek.kt
@@ -0,0 +1,49 @@
+package com.pusher.chatkit
+
+import com.google.common.truth.Truth.*
+import com.pusher.chatkit.Rooms.GENERAL
+import com.pusher.chatkit.Users.ALICE
+import com.pusher.chatkit.Users.PUSHERINO
+import com.pusher.chatkit.cursors.Cursor
+import com.pusher.chatkit.rooms.RoomSubscriptionEvent
+import com.pusher.chatkit.test.FutureValue
+import com.pusher.chatkit.test.InstanceActions.newRoom
+import com.pusher.chatkit.test.InstanceActions.newUsers
+import com.pusher.chatkit.test.InstanceSupervisor
+import com.pusher.chatkit.test.InstanceSupervisor.setUpInstanceWith
+import com.pusher.platform.network.wait
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+
+class CursorsSpek : Spek({
+
+    afterEachTest(InstanceSupervisor::tearDownInstance)
+    afterEachTest(::closeChatManagers)
+
+    describe("ChatManager with cursors") {
+
+        it("notifies when '$ALICE' reads messages") {
+            setUpInstanceWith(newUsers(PUSHERINO, ALICE), newRoom(GENERAL, PUSHERINO, ALICE))
+
+            var receivedCursor by FutureValue<Cursor>()
+            val pusherino = chatFor(PUSHERINO).connect().wait().assumeSuccess()
+            val alice = chatFor(ALICE).connect().wait().assumeSuccess()
+
+            val messageId = pusherino.sendMessage(pusherino.generalRoom, "Hey there").wait().assumeSuccess()
+
+            pusherino.subscribeToRoom(pusherino.generalRoom) { event ->
+                if (event is RoomSubscriptionEvent.NewReadCursor) receivedCursor = event.cursor
+            }
+
+            alice.setReadCursor(alice.generalRoom, messageId).wait().assumeSuccess()
+
+            val cursor = receivedCursor
+            assertThat(cursor.position).isEqualTo(messageId)
+            assertThat(cursor.userId).isEqualTo(ALICE)
+            assertThat(cursor.roomId).isEqualTo(pusherino.generalRoom.id)
+        }
+
+    }
+
+})
