7aedd3512e0c976b60ee42cb610a6e7aad1d07d1
Michael Pye <mail@mdpye.co.uk>
2018-09-04 15:55:22 +0100

    Move translation of UserSubEvent -> ChatManager event in to ChatManager
    The UserSubscription/Service should not know about entities higher up
    the stack (e.g. ChatManager). It is the responsibility of the
    ChatManager, which does know about UserService (because it creates it!)
    to encapsulate this transformation.

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/users/UserSubscriptionEvent.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/users/UserSubscriptionEvent.kt
@@ -2,8 +2,7 @@ package com.pusher.chatkit.users
 
 import com.pusher.chatkit.rooms.Room
 
-internal sealed class UserSubscriptionEvent {
-
+sealed class UserSubscriptionEvent {
     internal data class InitialState(val rooms: List<Room>, val currentUser: User) : UserSubscriptionEvent()
     internal data class AddedToRoomEvent(val room: Room) : UserSubscriptionEvent()
     internal data class RoomUpdatedEvent(val room: Room) : UserSubscriptionEvent()
@@ -13,5 +12,5 @@ internal sealed class UserSubscriptionEvent {
     internal data class JoinedRoomEvent(val roomId: Int, val userId: String) : UserSubscriptionEvent()
     internal data class StartedTyping(val userId: String, val roomId: Int) : UserSubscriptionEvent()
     internal data class StoppedTyping(val userId: String, val roomId: Int) : UserSubscriptionEvent()
-
+    internal data class ErrorOccurred(val error: elements.Error) : UserSubscriptionEvent()
 }
