c69ab68261df4992e735ca825626295a5b48fba8
Michael Pye <mail@mdpye.co.uk>
2018-09-14 15:40:34 +0100

    Remove unused HasUser interface

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/cursors/Cursor.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/cursors/Cursor.kt
@@ -1,14 +1,13 @@
 package com.pusher.chatkit.cursors
 
 import com.pusher.chatkit.rooms.HasRoom
-import com.pusher.chatkit.users.HasUser
 import com.pusher.chatkit.util.dateFormat
 import java.util.*
 
 data class Cursor(
-    override val userId: String,
+    val userId: String,
     override val roomId: Int,
     val position: Int,
     val updatedAt: String = dateFormat.format(Date()),
     val type: Int = 0
-) : HasUser, HasRoom
+) : HasRoom
