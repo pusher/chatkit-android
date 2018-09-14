bc62bd1f3df166068053d6ae34a13859d96cf088
Michael Pye <mail@mdpye.co.uk>
2018-09-14 15:40:34 +0100

    Remove unused HasUser interface

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatEvent.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatEvent.kt
@@ -1,11 +1,10 @@
 package com.pusher.chatkit
 
 import com.google.gson.JsonElement
-import com.pusher.chatkit.users.HasUser
 
 internal data class ChatEvent(
     val eventName: String,
-    override val userId: String = "",
+    val userId: String = "",
     val timestamp: String,
     val data: JsonElement
-) : HasUser
+)
--- chatkit-core/src/main/kotlin/com/pusher/chatkit/messages/Message.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/messages/Message.kt
@@ -2,17 +2,16 @@ package com.pusher.chatkit.messages
 
 import com.pusher.chatkit.files.Attachment
 import com.pusher.chatkit.rooms.HasRoom
-import com.pusher.chatkit.users.HasUser
 import com.pusher.chatkit.users.User
 
 data class Message(
     val id: Int,
-    override val userId: String,
+    val userId: String,
     override val roomId: Int,
     val text: String? = null,
     val attachment: Attachment? = null,
     val createdAt: String,
     val updatedAt: String
-) : HasRoom, HasUser {
+) : HasRoom {
     var user: User? = null
 }
