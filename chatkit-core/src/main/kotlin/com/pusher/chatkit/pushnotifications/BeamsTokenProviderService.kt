03b9e7c89bd174e7c98f5917365c855a138e5409
Michael Pye <mail@mdpye.co.uk>
2018-11-26 14:14:24 +0000

    URL encoding for argument

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/pushnotifications/BeamsTokenProviderService.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/pushnotifications/BeamsTokenProviderService.kt
@@ -4,19 +4,20 @@ import com.pusher.chatkit.PlatformClient
 import com.pusher.chatkit.util.parseAs
 import com.pusher.platform.RequestOptions
 import com.pusher.util.Result
+import java.net.URLEncoder
 
 data class BeamsTokenProviderResponse(val token: String?)
 
 class BeamsTokenProviderService(
         private val beamsTokenProviderClient: PlatformClient
 ) {
-  fun fetchToken(userId: String): Result<BeamsTokenProviderResponse, elements.Error> {
-    return beamsTokenProviderClient.doRequest<BeamsTokenProviderResponse>(
-            options = RequestOptions(
-                    path = "/beams-tokens?user_id=$userId",
-                    method = "GET"
-            ),
-            responseParser = { it.parseAs() }
-    )
-  }
+    fun fetchToken(userId: String): Result<BeamsTokenProviderResponse, elements.Error> {
+        return beamsTokenProviderClient.doRequest(
+                options = RequestOptions(
+                        path = "/beams-tokens?user_id=${URLEncoder.encode(userId, "UTF-8")}",
+                        method = "GET"
+                ),
+                responseParser = { it.parseAs<BeamsTokenProviderResponse>() }
+        )
+    }
 }
