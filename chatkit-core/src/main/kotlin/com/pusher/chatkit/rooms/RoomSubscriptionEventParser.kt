ef57a4d53c9629e3df414cc389ba4a5c65e9b387
Vivan Kumar <vivangkumar@gmail.com>
2019-05-08 17:24:46 +0100

    Implement MessageDeleted hook

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/RoomSubscriptionEventParser.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/RoomSubscriptionEventParser.kt
@@ -61,6 +61,7 @@ internal object RoomSubscriptionEventParserV3 : DataParser<RoomSubscriptionEvent
                     "new_message" ->
                         parseAs<V3MessageBody>().map(RoomSubscriptionEvent::NewMultipartMessage)
                     "is_typing" -> parseAs<RoomSubscriptionEvent.UserIsTyping>()
+                    "message_deleted" -> parseAs<RoomSubscriptionEvent.MessageDeleted>()
                     else -> Errors.other("Invalid event name: $eventName").asFailure()
                 }.map { it } // generics :O
             } catch (e: Throwable) {
