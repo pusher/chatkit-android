cf4faa349d1c8e18bcbc2e30d546ce1c7b7cb9b2
Danielle Vass <danielle.vass@pusher.com>
2019-10-15 14:21:30 +0100

    wip

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/CurrentUser.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/CurrentUser.kt
@@ -16,6 +16,9 @@ import com.pusher.util.Result
 import elements.Error
 import elements.Subscription
 
+/**
+ * The interface for what the current user can do in Chatkit asynchronously using callbacks.
+ */
 @Suppress("MemberVisibilityCanBePrivate") // Entry points
 class CurrentUser(
         private val syncCurrentUser: SynchronousCurrentUser
@@ -37,47 +40,101 @@ class CurrentUser(
         get() = syncCurrentUser.rooms
 
 
+    /**
+     * Returns a callback with a list of all the users on the instance if successful.
+     * If unsuccessful an [Error] will be returned.
+     */
     fun users(callback: (Result<List<User>, Error>) -> Unit) =
             makeCallback({ syncCurrentUser.users }, callback)
 
+    /**
+     * Returns whether the current user is subscribed to the [roomId]
+     */
     fun isSubscribedToRoom(roomId: String): Boolean =
             syncCurrentUser.isSubscribedToRoom(roomId)
 
+    /**
+     * Returns whether the current user is subscribed to the [room]
+     */
     fun isSubscribedToRoom(room: Room): Boolean =
             syncCurrentUser.isSubscribedToRoom(room)
 
+    /**
+     * Updates the current user with the properties of [newUser]
+     */
     fun updateWithPropertiesOf(newUser: CurrentUser) =
             syncCurrentUser.updateWithPropertiesOf(newUser.syncCurrentUser)
 
+    /**
+     * Sets the read cursor for the cur rent user in [room] to [position].
+     * The [position] is the id of the last message that the current user has read.
+     */
     fun setReadCursor(room: Room, position: Int) {
         Futures.schedule { syncCurrentUser.setReadCursor(room.id, position) }
     }
 
+    /**
+     * Sets the read cursor for the current user in [roomId] to [position].
+     * The [position] is the id of the last message that the current user has read.
+     */
     fun setReadCursor(roomId: String, position: Int) {
         Futures.schedule { syncCurrentUser.setReadCursor(roomId, position) }
     }
 
+    /**
+     * Returns the read [Cursor] for the current user in [roomId] if successful If unsuccessful
+     * an [Error] is returned.
+     */
     fun getReadCursor(roomId: String): Result<Cursor, Error> =
             syncCurrentUser.getReadCursor(roomId)
-
+    /**
+     * Returns the read [Cursor] for the current user in [room] if successful If unsuccessful
+     * an [Error] is returned.
+     */
     fun getReadCursor(room: Room): Result<Cursor, Error> =
             syncCurrentUser.getReadCursor(room.id)
 
+    /**
+     * Returns the read [Cursor] for the [userId] in [roomId] if successful If unsuccessful
+     * an [Error] is returned.
+     */
     fun getReadCursor(roomId: String, userId: String): Result<Cursor, Error> =
             syncCurrentUser.getReadCursor(roomId, userId)
 
+    /**
+     * Returns the read [Cursor] for the [user] in [room] if successful If unsuccessful
+     * an [Error] is returned.
+     */
     fun getReadCursor(room: Room, user: User): Result<Cursor, Error> =
             syncCurrentUser.getReadCursor(room.id, user.id)
 
+    /**
+     * Adds the [userIds] to the [roomId]. The [callback] returns void if successful,
+     * and an [Error] if unsuccessful.
+     */
     fun addUsersToRoom(roomId: String, userIds: List<String>, callback: (Result<Unit, Error>) -> Unit) =
             makeCallback({ syncCurrentUser.addUsersToRoom(roomId, userIds) }, callback)
 
+    /**
+     * Removes the [userIds] from the [roomId]. The [callback] returns void if successful,
+     * and an [Error] if unsuccessful.
+     */
     fun removeUsersFromRoom(
             roomId: String,
             userIds: List<String>,
             callback: (Result<Unit, Error>) -> Unit
     ) = makeCallback({ syncCurrentUser.removeUsersFromRoom(roomId, userIds) }, callback)
 
+    /**
+     * Create a new room with:
+     * - [id] - optional - a uniquely identifying String, if not provided a randomly generated unique string will be created for you
+     * - [name] - a String to label the room
+     * - [pushNotificationTitleOverride] - optional - a string that appears on the push notification title for this room, by default the title is the room name
+     * - [isPrivate] - optional - a boolean to determine if the room is available for others to join, by default the room will be not be private
+     * - [customData] - optional - a [CustomData] object with any extra information you'd like to save with the new room e.g. mapOf("description" to "some extra description about the room")
+     * - [userIds] - optional - a list of the userIds of users who will be added to the room
+     * - [callback] - returns a [Result] with the [Room] if success, or an [Error] if something went wrong
+     */
     @JvmOverloads
     fun createRoom(
             id: String? = null,
@@ -89,6 +146,14 @@ class CurrentUser(
             callback: (Result<Room, Error>) -> Unit
     ) = makeCallback({ syncCurrentUser.createRoom(id, name, pushNotificationTitleOverride, isPrivate, customData, userIds) }, callback)
 
+    /**
+     * Update an existing [room]'s properties:
+     * - [name] - a String to label the room
+     * - [pushNotificationTitleOverride] - optional - a string that appears on the push notification title for this room, by default the title is the room name
+     * - [isPrivate]- optional - a boolean to determine if the room is available for others to join, by default the room will be not be private
+     * - [customData] - optional - a [CustomData] object with any extra information you'd like to save with the new room e.g. mapOf("description" to "some extra description about the room")
+     * - [callback] - returns a [Result] with the [Room] if success, or an [Error] if something went wrong
+     */
     @JvmOverloads
     fun updateRoom(
             room: Room,
@@ -99,6 +164,18 @@ class CurrentUser(
             callback: (Result<Unit, Error>) -> Unit
     ) = makeCallback({ syncCurrentUser.updateRoom(room, name, pushNotificationTitleOverride, isPrivate, customData) }, callback)
 
+    /**
+     * Update an existing room's properties by using the [roomId]:
+     * - [name] - a String to label the room
+     * - [pushNotificationTitleOverride] - optional - a string that appears on the push notification
+     *   title for this room, by default the title is the room name
+     * - [isPrivate]- optional - a boolean to determine if the room is available for others to join,
+     *   by default the room will be not be private
+     * - [customData] - optional - a [CustomData] object with any extra information you'd like to save
+     * with the new room e.g. mapOf("description" to "some extra description about the room")
+     * - [callback] - will be called with a [Result] when the operation is complete. You will receive
+     * a [Unit] if the operation was successful, or an [Error] informing you of what went wrong
+     */
     @JvmOverloads
     fun updateRoom(roomId: String,
                    name: String,
@@ -108,9 +185,18 @@ class CurrentUser(
                    callback: (Result<Unit, Error>) -> Unit
     ) = makeCallback({ syncCurrentUser.updateRoom(roomId, name, pushNotificationTitleOverride, isPrivate, customData) }, callback)
 
+    /**
+     * Delete the [room]. The [callback] will be called with a [Result] when the operation is complete.
+     * If the operation was successful you will receive a [String] which contains ?,
+     * or an [Error] informing you of what went wrong.
+     * This is a soft delete, which means you cannot reuse the roomId later.
+     */
     fun deleteRoom(room: Room, callback: (Result<String, Error>) -> Unit) =
             makeCallback({ syncCurrentUser.deleteRoom(room) }, callback)
 
+    /**
+     * @see #deleteRoom
+     */
     fun deleteRoom(roomId: String, callback: (Result<String, Error>) -> Unit) =
             makeCallback({ syncCurrentUser.deleteRoom(roomId) }, callback)
 
