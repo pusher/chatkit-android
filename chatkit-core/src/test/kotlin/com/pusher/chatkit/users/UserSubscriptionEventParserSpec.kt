c230ad339662539ed527b63a7d57c2a66840d5c1
Marek Urbaniak <marek.urbaniak@gmail.com>
2019-12-05 11:11:50 +0000

    Add parsing membership in AddedToRoom of user's subscription
    Simplify UserSubscriptionEventParserSpec as there is no common
    "given" state (which is being changed in "when" steps), just results
    of parsing ("when") and their verifications ("then"s).

--- chatkit-core/src/test/kotlin/com/pusher/chatkit/users/UserSubscriptionEventParserSpec.kt
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/users/UserSubscriptionEventParserSpec.kt
@@ -3,49 +3,44 @@ package com.pusher.chatkit.users
 import com.google.common.truth.Truth.assertThat
 import com.pusher.chatkit.users.UserSubscriptionEvent.AddedToRoomEvent
 import com.pusher.chatkit.users.UserSubscriptionEvent.InitialState
-import com.pusher.util.Result
-import elements.Error
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 object UserSubscriptionEventParserSpec : Spek({
     describe("when parsing initial state example event from the docs") {
-        lateinit var result : Result<UserSubscriptionEvent, Error>
-
-        beforeEachTest {
-            result = UserSubscriptionEventParser(readTestJson("initial_state-docs"))
-        }
+        val initialStateEvent = UserSubscriptionEventParser(readTestJson("initial_state-docs"))
+                .successOrThrow() as InitialState
 
         // TODO: add tests around other bits like unread counts, cursors etc
 
         it("then result's memberships will contain valid information") {
-            val initialStateEvent = result.successOrThrow() as InitialState
+            val memberships = initialStateEvent.memberships
 
-            val membership = initialStateEvent.memberships
+            assertThat(memberships.size).isEqualTo(2)
 
-            assertThat(membership.size).isEqualTo(2)
+            assertThat(memberships[0].roomId).isEqualTo("cool-room-1")
+            assertThat(memberships[0].userIds.size).isEqualTo(2)
+            assertThat(memberships[0].userIds[0]).isEqualTo("jean")
+            assertThat(memberships[0].userIds[1]).isEqualTo("ham")
 
-            assertThat(membership[0].roomId).isEqualTo("cool-room-1")
-            assertThat(membership[0].userIds.size).isEqualTo(2)
-            assertThat(membership[0].userIds[0]).isEqualTo("jean")
-            assertThat(membership[0].userIds[1]).isEqualTo("ham")
-
-            assertThat(membership[1].roomId).isEqualTo("party-room")
-            assertThat(membership[1].userIds.size).isEqualTo(1)
-            assertThat(membership[1].userIds[0]).isEqualTo("ham")
+            assertThat(memberships[1].roomId).isEqualTo("party-room")
+            assertThat(memberships[1].userIds.size).isEqualTo(1)
+            assertThat(memberships[1].userIds[0]).isEqualTo("ham")
         }
 
     }
     describe("when parsing added to room example event from the docs") {
-        lateinit var result : Result<UserSubscriptionEvent, Error>
-
-        beforeEachTest {
-            result = UserSubscriptionEventParser(readTestJson("added_to_room-docs"))
-        }
+        val addedToRoomEvent = UserSubscriptionEventParser(readTestJson("added_to_room-docs"))
+                .successOrThrow() as AddedToRoomEvent
 
         it("then result's room will have unread count relayed from read state") {
-            val addedToRoomEvent = result.successOrThrow() as AddedToRoomEvent
             assertThat(addedToRoomEvent.room.unreadCount).isEqualTo(15)
+        }
+        it("then result's memberships will contain valid information") {
+            val membership = addedToRoomEvent.memberships
+            assertThat(membership.roomId).isEqualTo("cool-room-2")
+            assertThat(membership.userIds.size).isEqualTo(1)
+            assertThat(membership.userIds[0]).isEqualTo("ham")
         }
 
     }
