1270a326ec0f71490bdcd98450010c1c274eda1f
Marek Urbaniak <marek.urbaniak@gmail.com>
2019-12-23 13:57:32 +0000

    Remove room membership subscription
    The room membership subscription has been removed in v7 of BE core.
    
    Add handling membership related events of user subscription for
    mutating RoomStore and translation of the events for further
    processing.

--- chatkit-core/src/test/kotlin/com/pusher/chatkit/RoomTestUtil.kt
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/RoomTestUtil.kt
@@ -2,11 +2,14 @@ package com.pusher.chatkit
 
 import com.pusher.chatkit.rooms.Room
 
-fun simpleRoom(id: String, name: String, isPrivate: Boolean, customData: CustomData?,
-               unreadCount : Int? = null) =
+fun simpleRoom(id: String, name: String, isPrivate: Boolean = false, customData: CustomData? = null,
+               unreadCount : Int? = null,
+               // Gson puts null here but for the tests empty set should be fine too
+               memberUserIds: Set<String> = emptySet()
+) =
         Room(id, "ham", name, null, isPrivate, customData, unreadCount,
                 "2017-04-13T14:10:38Z",
                 "2017-04-13T14:10:38Z",
                 "2017-04-13T14:10:38Z",
                 "2017-04-13T14:10:38Z",
-                setOf()) // Gson puts null here but for the tests empty list should be fine too
+                memberUserIds)
