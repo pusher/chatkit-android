453a695368424e79a94c46af7fb6e950f0b1f7e2
Marek Urbaniak <marek.urbaniak@gmail.com>
2020-01-08 12:14:30 +0000

    Add plumbing for providing test doubles of PlatformClient
    The approach has been consulted with Mike.

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/PlatformClient.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/PlatformClient.kt
@@ -12,6 +12,15 @@ import com.pusher.util.Result
 import elements.Error
 import elements.Subscription
 
+internal interface PlatformClientFactory {
+    fun createPlatformClient(instance: Instance, tokenProvider: TokenProvider): PlatformClient
+}
+
+internal class DefaultPlatformClientFactory : PlatformClientFactory {
+    override fun createPlatformClient(instance: Instance, tokenProvider: TokenProvider) =
+        PlatformClient(instance, tokenProvider)
+}
+
 class PlatformClient(
         private val platformInstance: Instance,
         private val tokenProvider: TokenProvider
