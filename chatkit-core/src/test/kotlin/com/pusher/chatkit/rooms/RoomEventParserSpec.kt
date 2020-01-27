01ad206bc3ce7111fa3289d3a6d03cf3cb1d954c
Danielle Vass <danielle.vass@pusher.com>
2020-01-27 15:42:40 +0000

    Implement feedback on RoomEventParserSpec tests

--- chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/RoomEventParserSpec.kt
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/RoomEventParserSpec.kt
@@ -1,9 +1,10 @@
 package com.pusher.chatkit.rooms
 
-import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
 import com.pusher.chatkit.TestFileReader
 import com.pusher.chatkit.rooms.api.CreateRoomResponse
 import com.pusher.chatkit.rooms.api.JoinRoomResponse
+import com.pusher.chatkit.rooms.api.JoinedRoomApiType
 import com.pusher.chatkit.util.parseAs
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
@@ -13,64 +14,66 @@ object RoomEventParserSpec : Spek({
 
     describe("when create room response from the docs is parsed") {
 
-        val result = testFileReader.readTestFile("room_created-docs.json")
+        val result = testFileReader.readTestFile("room_created_response-docs.json")
                 .parseAs<CreateRoomResponse>().successOrThrow()
 
         it("then the result has expected room") {
-            val room = result.room
+            val expectedRoom = JoinedRoomApiType(
+                    "ac43dfef",
+                    "alice",
+                    "Chatkit chat",
+                    null,
+                    false,
+                    mapOf("highlight_color" to "blue"),
+                    "2020-01-08T14:55:10Z",
+                    "2017-03-23T11:36:42Z",
+                    "2017-03-23T11:36:42Z",
+                    null)
 
-            Truth.assertThat(room.id).isEqualTo("ac43dfef")
-            Truth.assertThat(room.name).isEqualTo("Chatkit chat")
-            Truth.assertThat(room.createdById).isEqualTo("alice")
-            Truth.assertThat(room.pushNotificationTitleOverride).isEqualTo(null)
-            Truth.assertThat(room.private).isEqualTo(false)
-            Truth.assertThat(room.lastMessageAt).isEqualTo("2020-01-08T14:55:10Z")
-            Truth.assertThat(room.createdAt).isEqualTo("2017-03-23T11:36:42Z")
-            Truth.assertThat(room.updatedAt).isEqualTo("2017-03-23T11:36:42Z")
-            Truth.assertThat(room.customData).isEqualTo(mapOf("highlight_color" to "blue"))
+            assertThat(result.room).isEqualTo(expectedRoom)
         }
 
         it("creates valid membership") {
             val membership = result.membership
 
-            Truth.assertThat(membership.roomId).isEqualTo("ac43dfef")
-            Truth.assertThat(membership.userIds.size).isEqualTo(2)
-            Truth.assertThat(membership.userIds[0]).isEqualTo("alice")
-            Truth.assertThat(membership.userIds[1]).isEqualTo("carol")
+            assertThat(membership.roomId).isEqualTo("ac43dfef")
+            assertThat(membership.userIds.size).isEqualTo(2)
+            assertThat(membership.userIds[0]).isEqualTo("alice")
+            assertThat(membership.userIds[1]).isEqualTo("carol")
         }
 
     }
 
     describe("when joining a room example from the docs") {
 
-        val result = testFileReader.readTestFile("room_joined-docs.json")
+        val result = testFileReader.readTestFile("room_joined_response-docs.json")
                 .parseAs<JoinRoomResponse>().successOrThrow()
 
         it("creates a valid room") {
-            val room = result.room
+            val expectedRoom = JoinedRoomApiType(
+                    "ac43dfef",
+                    "alice",
+                    "Chatkit chat",
+                    null,
+                    false,
+                    mapOf("highlight_color" to "blue"),
+                    "2020-01-08T14:55:10Z",
+                    "2017-03-23T11:36:42Z",
+                    "2017-03-23T11:36:42Z",
+                    null)
 
-            Truth.assertThat(room.id).isEqualTo("ac43dfef")
-            Truth.assertThat(room.name).isEqualTo("Chatkit chat")
-            Truth.assertThat(room.createdById).isEqualTo("alice")
-            Truth.assertThat(room.pushNotificationTitleOverride).isEqualTo(null)
-            Truth.assertThat(room.private).isEqualTo(false)
-            Truth.assertThat(room.lastMessageAt).isEqualTo("2020-01-08T14:55:10Z")
-            Truth.assertThat(room.createdAt).isEqualTo("2017-03-23T11:36:42Z")
-            Truth.assertThat(room.updatedAt).isEqualTo("2017-03-23T11:36:42Z")
-            Truth.assertThat(room.customData).isEqualTo(mapOf("highlight_color" to "blue"))
+            assertThat(result.room).isEqualTo(expectedRoom)
         }
 
         it("creates valid membership") {
             val membership = result.membership
 
-            Truth.assertThat(membership.roomId).isEqualTo("ac43dfef")
-            Truth.assertThat(membership.userIds.size).isEqualTo(2)
-            Truth.assertThat(membership.userIds[0]).isEqualTo("alice")
-            Truth.assertThat(membership.userIds[1]).isEqualTo("carol")
+            assertThat(membership.roomId).isEqualTo("ac43dfef")
+            assertThat(membership.userIds.size).isEqualTo(2)
+            assertThat(membership.userIds[0]).isEqualTo("alice")
+            assertThat(membership.userIds[1]).isEqualTo("carol")
         }
 
     }
-
-
 
 })
