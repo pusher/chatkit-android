7e81234e94fc78456de9552e221c66c0472c58fb
Danielle Vass <daniellevass@users.noreply.github.com>
2020-01-30 10:49:09 +0000

    Add more room parser tests (#150)
    * Add tests for room event parser
    
    * Update chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/RoomEventParserSpec.kt
    
    Co-Authored-By: Marek <marek.urbaniak@gmail.com>
    
    * Update chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/RoomEventParserSpec.kt
    
    Co-Authored-By: Marek <marek.urbaniak@gmail.com>
    
    * Implement feedback on RoomEventParserSpec tests
    
    * RoomEventParser tests against an expected membership
    
    * Implement suggested refactoring
    
    * Add another test for joinable rooms api type
    
    * Apply Mareks patch
    
    Co-authored-by: Marek <marek.urbaniak@gmail.com>

---
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/GetJoinableRoomsParserSpec.kt
@@ -0,0 +1,31 @@
+package com.pusher.chatkit.rooms
+
+import com.google.common.truth.Truth.assertThat
+import com.pusher.chatkit.TestFileReader
+import com.pusher.chatkit.rooms.api.*
+import com.pusher.chatkit.util.parseAs
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+
+object GetJoinableRoomsParserSpec : Spek({
+    val testFileReader = TestFileReader("/json/rooms")
+
+    describe("when get joinable rooms response from the docs is parsed") {
+        val result = testFileReader.readTestFile("get_joinable_rooms_response-docs.json")
+                .parseAs<JoinableRoomsResponse>().successOrThrow()
+
+        it("then the result has expected room") {
+            val expectedRoom = NotJoinedRoomApiType(
+                    id = "ac43dfef",
+                    createdById = "alice",
+                    name = "Chatkit chat",
+                    pushNotificationTitleOverride = null,
+                    customData = mapOf("highlight_color" to "blue"),
+                    createdAt = "2017-03-23T11:36:42Z",
+                    updatedAt = "2017-03-23T11:36:42Z",
+                    deletedAt = null)
+
+            assertThat(result.rooms).containsExactly(expectedRoom)
+        }
+    }
+})
