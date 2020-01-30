497f9846db2ade184fd26e56ea7e11b0e72cff08
Danielle Vass <danielle.vass@pusher.com>
2020-01-30 14:05:34 +0000

    Add e2e process test for joining a room

--- chatkit-core/src/integrationTest/kotlin/com/pusher/chatkit/JoinRoomTests.kt
+++ chatkit-core/src/integrationTest/kotlin/com/pusher/chatkit/JoinRoomTests.kt
@@ -11,68 +11,107 @@ import com.pusher.chatkit.test.InstanceActions.newUsers
 import com.pusher.chatkit.test.InstanceSupervisor.setUpInstanceWith
 import com.pusher.chatkit.test.InstanceSupervisor.tearDownInstance
 import com.pusher.chatkit.util.FutureValue
-import com.pusher.util.Result.Failure
-import com.pusher.util.Result.Success
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
+import com.pusher.util.Result
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 object JoinRoomTests : Spek({
-    beforeEachTest(::tearDownInstance)
-    afterEachTest(::closeChatManagers)
+    afterGroup(::tearDownInstance)
+    afterGroup(::closeChatManagers)
 
-    describe("currentUser '$PUSHERINO'") {
+    describe("currentUser '$PUSHERINO' joins room") {
 
-        it("joins room") {
-            setUpInstanceWith(
-                    createDefaultRole(),
-                    newUsers(PUSHERINO),
-                    newRoom(GENERAL))
+        setUpInstanceWith(
+                createDefaultRole(),
+                newUsers(PUSHERINO),
+                newRoom(GENERAL))
 
-            val pusherino = chatFor(PUSHERINO).connect().assumeSuccess()
-            val room = pusherino.joinRoom(GENERAL)
+        val pusherino = chatFor(PUSHERINO).connect().assumeSuccess()
+        val room = pusherino.joinRoom(GENERAL)
 
-            check(room is Success) { (room as? Failure)?.error as Any }
-            assertThat(pusherino.rooms).contains(pusherino.generalRoom)
-        }
+        check(room is Result.Success) { (room as? Result.Failure)?.error as Any }
+        assertThat(pusherino.rooms).contains(pusherino.generalRoom)
+    }
 
-        it ("joins room with other members") {
-            setUpInstanceWith(
-                    createDefaultRole(),
-                    newUsers(PUSHERINO, ALICE),
-                    newRoom(GENERAL, ALICE))
+    describe ("currentUser '$PUSHERINO' joins room with other members") {
+        setUpInstanceWith(
+                createDefaultRole(),
+                newUsers(PUSHERINO, ALICE),
+                newRoom(GENERAL, ALICE))
 
-            var pusherinoJoinedRoomEvent by FutureValue<ChatEvent.AddedToRoom>()
-            val pusherino = chatFor(PUSHERINO).connect{ event ->
-                when (event) {
-                    is ChatEvent.AddedToRoom -> {
-                        pusherinoJoinedRoomEvent = event
-                    }
+        var pusherinoJoinedRoomEvent by FutureValue<ChatEvent.AddedToRoom>()
+        val pusherino = chatFor(PUSHERINO).connect{ event ->
+            when (event) {
+                is ChatEvent.AddedToRoom -> {
+                    pusherinoJoinedRoomEvent = event
                 }
-            }.assumeSuccess()
+            }
+        }.assumeSuccess()
 
-            val room = pusherino.joinRoom(GENERAL).assumeSuccess()
+        val room = pusherino.joinRoom(GENERAL).assumeSuccess()
 
-            assertThat(room.memberUserIds)
-                    .containsExactly(ALICE, PUSHERINO, SUPER_USER)
+        assertThat(room.memberUserIds)
+                .containsExactly(ALICE, PUSHERINO, SUPER_USER)
+        assertThat(pusherino.rooms[0].memberUserIds)
+                .containsExactly(ALICE, PUSHERINO, SUPER_USER)
+        assertThat(pusherinoJoinedRoomEvent.room.memberUserIds)
+                .containsExactly(ALICE, PUSHERINO, SUPER_USER)
+    }
+
+    describe("currentUser '$PUSHERINO' leaves room") {
+        setUpInstanceWith(createDefaultRole(), newUsers(PUSHERINO), newRoom(GENERAL, PUSHERINO))
+
+        val pusherino = chatFor(SUPER_USER).connect().assumeSuccess()
+        val generalRoom = pusherino.generalRoom
+
+        val room = pusherino.leaveRoom(generalRoom)
+
+        check(room is Result.Success) { (room as? Result.Failure)?.error as Any }
+        assertThat(pusherino.rooms).doesNotContain(generalRoom)
+    }
+
+    describe( "current user '$PUSHERINO") {
+        setUpInstanceWith(
+                createDefaultRole(),
+                newUsers(PUSHERINO, ALICE),
+                newRoom(GENERAL, ALICE)
+        )
+
+        var addedToRoomEvent by FutureValue<ChatEvent.AddedToRoom>()
+        val pusherino = chatFor(PUSHERINO).connect { event ->
+            if (event is ChatEvent.AddedToRoom) addedToRoomEvent = event
+        }.assumeSuccess()
+
+        it( "has no current rooms") {
+            assertThat(pusherino.rooms).isEmpty()
+        }
+
+        it ("has one joinable rooms") {
+            val joinableRooms = pusherino.getJoinableRooms().assumeSuccess()
+
+            assertThat(joinableRooms.size).isEqualTo(1)
+            assertThat(joinableRooms[0].id).isEqualTo(GENERAL)
+            assertThat(joinableRooms[0].memberUserIds).isEmpty()
+        }
+
+        it ("joins room '$GENERAL' with accurate membership") {
+            val generalRoom = pusherino.joinRoom(GENERAL).assumeSuccess()
+            assertThat(generalRoom.memberUserIds)
+                    .containsExactly(PUSHERINO, ALICE, SUPER_USER)
+
+            assertThat(pusherino.rooms).contains(pusherino.generalRoom)
             assertThat(pusherino.rooms[0].memberUserIds)
-                    .containsExactly(ALICE, PUSHERINO, SUPER_USER)
-            assertThat(pusherinoJoinedRoomEvent.room.memberUserIds)
-                    .containsExactly(ALICE, PUSHERINO, SUPER_USER)
+                    .containsExactly(PUSHERINO, ALICE, SUPER_USER)
+
+            assertThat(addedToRoomEvent.room.id).isEqualTo(GENERAL)
+            assertThat(addedToRoomEvent.room.memberUserIds)
+                    .containsExactly(PUSHERINO, ALICE, SUPER_USER)
         }
 
-        it("leaves room") {
-            setUpInstanceWith(createDefaultRole(), newUsers(PUSHERINO), newRoom(GENERAL, PUSHERINO))
-
-            val pusherino = chatFor(SUPER_USER).connect().assumeSuccess()
-            val generalRoom = pusherino.generalRoom
-
-            val room = pusherino.leaveRoom(generalRoom)
-
-            check(room is Success) { (room as? Failure)?.error as Any }
-            assertThat(pusherino.rooms).doesNotContain(generalRoom)
+        it ("leaves room '$GENERAL'") {
+            pusherino.leaveRoom(GENERAL).assumeSuccess()
+            assertThat(pusherino.rooms).isEmpty()
         }
-
 
     }
 })
