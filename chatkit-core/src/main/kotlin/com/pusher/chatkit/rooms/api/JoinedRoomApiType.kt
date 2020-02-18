02da9a40aa00bfaeb20985806921f433bae76964
Danielle Vass <danielle.vass@pusher.com>
2020-02-18 11:10:59 +0000

    Remove commented import from JoinedRoomApiType

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/api/JoinedRoomApiType.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/api/JoinedRoomApiType.kt
@@ -2,8 +2,6 @@ package com.pusher.chatkit.rooms.api
 
 import com.pusher.chatkit.CustomData
 
-// import com.pusher.chatkit.CustomData
-
 internal data class JoinedRoomApiType(
     val id: String,
     val createdById: String,
