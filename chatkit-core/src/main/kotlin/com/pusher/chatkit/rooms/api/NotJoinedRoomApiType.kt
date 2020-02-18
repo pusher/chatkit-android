447de3a53b0fb5386faa03d1eb0c06ae152109c2
Danielle Vass <danielle.vass@pusher.com>
2020-02-18 11:11:24 +0000

    Remove commented import from NotJoinedRoomApiType

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/api/NotJoinedRoomApiType.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/api/NotJoinedRoomApiType.kt
@@ -2,8 +2,6 @@ package com.pusher.chatkit.rooms.api
 
 import com.pusher.chatkit.CustomData
 
-// import com.pusher.chatkit.CustomData
-
 internal data class NotJoinedRoomApiType(
     val id: String,
     val createdById: String,
