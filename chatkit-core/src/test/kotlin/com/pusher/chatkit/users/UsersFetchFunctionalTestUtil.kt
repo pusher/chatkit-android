fae5187bc12fa1e94f62511a68b1dc9103e6dfd0
Danielle Vass <danielle.vass@pusher.com>
2020-02-18 14:04:30 +0000

    Make the UsersFetchFUnctionalTestUtil use UserApiType

--- chatkit-core/src/test/kotlin/com/pusher/chatkit/users/UsersFetchFunctionalTestUtil.kt
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/users/UsersFetchFunctionalTestUtil.kt
@@ -1,18 +1,24 @@
 package com.pusher.chatkit.users
 
 import com.nhaarman.mockitokotlin2.KStubbing
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.anyOrNull
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.eq
 import com.pusher.chatkit.PlatformClient
+import com.pusher.chatkit.users.api.UserApiType
+import com.pusher.util.asFailure
 import elements.Error
+import org.mockito.ArgumentMatchers.startsWith
 
 internal fun usersFetchFailingWith(error: Error):
         KStubbing<PlatformClient>.(PlatformClient) -> Unit = { client ->
     on {
-//        client.doRequest<List<User>>(
-//                method = eq("GET"),
-//                path = startsWith("/users_by_ids"),
-//                body = anyOrNull(),
-//                responseParser = any()
-//        )
-    }
-    // doReturn error.asFailure()
+        client.doRequest<List<UserApiType>>(
+                method = eq("GET"),
+                path = startsWith("/users_by_ids"),
+                body = anyOrNull(),
+                responseParser = any()
+        )
+    } doReturn error.asFailure()
 }
