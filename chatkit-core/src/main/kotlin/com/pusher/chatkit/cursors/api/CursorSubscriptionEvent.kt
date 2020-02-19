d713213cd736ad771ea1c496283b4f207e18b114
Danielle Vass <danielle.vass@pusher.com>
2020-02-19 15:06:35 +0000

    Apply Mareks patch for ordering CursorSubscriptionEvents

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/cursors/api/CursorSubscriptionEvent.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/cursors/api/CursorSubscriptionEvent.kt
@@ -1,8 +1,8 @@
 package com.pusher.chatkit.cursors.api
 
 internal sealed class CursorSubscriptionEvent {
-    data class OnCursorSet(val cursor: CursorApiType) : CursorSubscriptionEvent()
     data class InitialState(val cursors: List<CursorApiType>) : CursorSubscriptionEvent()
+    data class OnCursorSet(val cursor: CursorApiType) : CursorSubscriptionEvent()
 }
 
 internal typealias CursorSubscriptionConsumer = (CursorSubscriptionEvent) -> Unit
