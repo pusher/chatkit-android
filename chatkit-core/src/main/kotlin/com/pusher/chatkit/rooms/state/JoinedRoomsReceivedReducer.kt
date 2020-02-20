9f5b200ea0e548d0df7246d1becfc62314f87e52
Danielle Vass <danielle.vass@pusher.com>
2020-02-20 15:49:11 +0000

    Add a JoinedRoomsReceivedReducer

---
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/state/JoinedRoomsReceivedReducer.kt
@@ -0,0 +1,22 @@
+package com.pusher.chatkit.rooms.state
+
+import com.pusher.chatkit.state.AuxiliaryStateUtil
+import com.pusher.chatkit.state.ChatkitState
+import org.reduxkotlin.reducerForActionType
+
+internal data class JoinedRoomsReceived(
+    val rooms: List<JoinedRoomInternalType>,
+    val unreadCounts: Map<String, Int>
+)
+
+internal val joinedRoomsReceivedReducer =
+    reducerForActionType<ChatkitState, JoinedRoomsReceived> { state, action ->
+        val joinedRooms = action.rooms.map {
+            Pair(it.id, it)
+        }.toMap()
+        val joinedRoomsState = JoinedRoomsState(joinedRooms, action.unreadCounts)
+        state.copy(
+            joinedRoomsState,
+            AuxiliaryStateUtil.applyReducerLastChangeJoinedRoomsState(state, joinedRoomsState)
+        )
+}
--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/AuxiliaryState.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/AuxiliaryState.kt
@@ -3,8 +3,19 @@ package com.pusher.chatkit.state
 import com.pusher.chatkit.rooms.state.JoinedRoomsState
 
 internal data class AuxiliaryState(
-        val version: Int,
-        val joinedRoomsReceived: ReducerLastChange<JoinedRoomsState>
+    val version: Int,
+    val joinedRoomsReceived: ReducerLastChange<JoinedRoomsState>
 )
 
 internal data class ReducerLastChange<T>(val version: Int, val data: T)
+
+internal class AuxiliaryStateUtil {
+    companion object {
+
+        internal fun applyReducerLastChangeJoinedRoomsState(state: ChatkitState, joinedRoomsState: JoinedRoomsState):
+                AuxiliaryState {
+            val version = state.auxiliaryState.version + 1
+            return state.auxiliaryState.copy(version, ReducerLastChange(version, joinedRoomsState))
+        }
+    }
+}
