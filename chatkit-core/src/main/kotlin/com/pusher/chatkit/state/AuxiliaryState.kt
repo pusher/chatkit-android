25a06c6290df8b9f1140142a58096d31fdeaa6cb
Danielle Vass <danielle.vass@pusher.com>
2020-02-20 15:30:31 +0000

    Refactor AuxiliaryState into it's own class

---
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/AuxiliaryState.kt
@@ -0,0 +1,10 @@
+package com.pusher.chatkit.state
+
+import com.pusher.chatkit.rooms.state.JoinedRoomsState
+
+internal data class AuxiliaryState(
+        val version: Int,
+        val joinedRoomsReceived: ReducerLastChange<JoinedRoomsState>
+)
+
+internal data class ReducerLastChange<T>(val version: Int, val data: T)
--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
@@ -6,10 +6,3 @@ internal data class ChatkitState(
     val joinedRoomsState: JoinedRoomsState,
     val auxiliaryState: AuxiliaryState
 )
-
-internal data class AuxiliaryState(
-    val version: Int,
-    val joinedRoomsReceived: ReducerLastChange<JoinedRoomsState>
-)
-
-internal data class ReducerLastChange<T>(val version: Int, val data: T)
