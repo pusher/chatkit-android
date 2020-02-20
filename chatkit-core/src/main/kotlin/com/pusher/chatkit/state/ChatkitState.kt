0e08a30ff5eb01de4662af55ec9128575e6ceca3
Danielle Vass <danielle.vass@pusher.com>
2020-02-20 15:28:29 +0000

    Remove auxiliary state things here - they'll come later

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
@@ -3,13 +3,5 @@ package com.pusher.chatkit.state
 import com.pusher.chatkit.rooms.state.JoinedRoomsState
 
 internal data class ChatkitState(
-    val joinedRoomsState: JoinedRoomsState,
-    val auxiliaryState: AuxiliaryState
+    val joinedRoomsState: JoinedRoomsState
 )
-
-internal data class AuxiliaryState(
-    val version: Int,
-    val joinedRoomsReceived: ReducerLastChange<JoinedRoomsState>
-)
-
-internal data class ReducerLastChange<T>(val version: Int, val data: T)
