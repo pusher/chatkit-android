d3ecac7e6b0921bd3e95c027f607635d0475370e
Danielle Vass <danielle.vass@pusher.com>
2020-02-20 11:07:17 +0000

    Create internal joined room internal type data class

---
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/internal/JoinedRoomInternalType.kt
@@ -0,0 +1,15 @@
+package com.pusher.chatkit.rooms.internal
+
+import com.pusher.chatkit.CustomData
+
+internal data class JoinedRoomInternalType(
+    val id: String,
+    val name: String,
+    val isPrivate: Boolean,
+    val pushNotificationTitleOverride: String?,
+    val customData: CustomData?,
+    val lastMessageAt: Long?,
+    val createdAt: Long,
+    val updatedAt: Long,
+    val deletedAt: Long?
+)
