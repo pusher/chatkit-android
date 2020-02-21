d865f82c5b1daafc2688ec38b71bcbc4e50482a5
Danielle Vass <danielle.vass@pusher.com>
2020-02-21 11:33:05 +0000

    Put back the Auxiliary State into Chatkit State

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/ChatkitState.kt
@@ -3,5 +3,6 @@ package com.pusher.chatkit.state
 import com.pusher.chatkit.rooms.state.JoinedRoomsState
 
 internal data class ChatkitState(
-    val joinedRoomsState: JoinedRoomsState?
+    val joinedRoomsState: JoinedRoomsState?,
+    val auxiliaryState: AuxiliaryState
 )
