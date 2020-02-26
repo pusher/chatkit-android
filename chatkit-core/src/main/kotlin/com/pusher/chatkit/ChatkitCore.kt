1f99de94b421768343aaf7cf5d5b069b8c51e8d3
Marek Urbaniak <marek.urbaniak@gmail.com>
2020-02-26 10:39:28 +0000

    Set up Koin around backend api

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitCore.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitCore.kt
@@ -1,13 +1,43 @@
 package com.pusher.chatkit
 
+import com.pusher.chatkit.api.createPlatformClientModule
+import com.pusher.chatkit.users.api.createUserSubscriberModule
 import com.pusher.util.Result
 import com.pusher.util.asSuccess
 import elements.Error
+import org.koin.core.KoinApplication
+import org.koin.core.module.Module
+import org.koin.dsl.koinApplication
+import org.koin.dsl.module
 
-class ChatkitCoreConnector(
-    private val instanceLocator: String,
+class ChatkitCoreConnector {
+
+    constructor(
+        instanceLocator: String,
+        dependencies: ChatkitDependencies
+    ) : this(instanceLocator, dependencies, overrideModules = emptyList())
+
+    internal constructor(
+        instanceLocator: String,
+        dependencies: ChatkitDependencies,
+        overrideModules: List<Module>
+    ) {
+        this.instanceLocator = instanceLocator
+        this.dependencies = dependencies
+
+        chatkitKoinApplication = koinApplication {
+            modules(
+                createPlatformClientModule(instanceLocator, dependencies),
+                module { single { dependencies.logger }},
+                createUserSubscriberModule()
+            )
+            modules(overrideModules)
+        }
+    }
+
+    private val instanceLocator: String
     private val dependencies: ChatkitDependencies
-) {
+    private val chatkitKoinApplication: KoinApplication
 
     fun connect(resultHandler: (Result<ChatkitCore, Error>) -> Unit) {
         // ...
--- chatkit-core/src/main/kotlin/com/pusher/chatkit/users/api/UserSubscriber.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/users/api/UserSubscriber.kt
@@ -4,6 +4,23 @@ import com.pusher.chatkit.PlatformClient
 import com.pusher.chatkit.subscription.loggingSubscription
 import com.pusher.platform.SubscriptionListeners
 import com.pusher.platform.logger.Logger
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+internal val createUserSubscriberModule: () -> Module = {
+    module {
+        factory { UserSubscriberFactory(get(), get()) }
+    }
+}
+
+internal class UserSubscriberFactory(
+    private val client: PlatformClient,
+    private val logger: Logger
+)  {
+
+    fun create(listeners: UserSubscriptionConsumer) = UserSubscriber(client, logger, listeners)
+
+}
 
 internal class UserSubscriber(
     private val client: PlatformClient,
