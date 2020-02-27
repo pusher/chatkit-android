ed77fe9a84ec21f9c45f3990c8b191dc4d12a105
Danielle Vass <danielle.vass@pusher.com>
2020-02-27 14:10:28 +0000

    Refactor JoinedRoom to assume we will always have a joinedRoomState

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/state/JoinedRoom.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/state/JoinedRoom.kt
@@ -9,25 +9,13 @@ internal data class JoinedRoom(
 )
 
 internal val joinedRoomReducer =
-    reducerForActionType<ChatkitState, JoinedRoom> { state, action ->
+        reducerForActionType<ChatkitState, JoinedRoom> { state, action ->
 
             val joinedRoom = Pair(action.room.id, action.room)
             val joinedRoomUnreadCount = Pair(action.room.id, action.unreadCount)
-            var joinedRoomsState = JoinedRoomsState(
-                    mapOf(joinedRoom),
-                    mapOf(joinedRoomUnreadCount)
-            )
 
-            if (state.joinedRoomsState != null) {
-                    val totalJoinedRooms = (state.joinedRoomsState.rooms +
-                            joinedRoom)
-                    val totalUnreadRooms = (state.joinedRoomsState.unreadCounts +
-                            joinedRoomUnreadCount)
-                    joinedRoomsState = JoinedRoomsState(
-                            totalJoinedRooms,
-                            totalUnreadRooms
-                    )
-            }
-
-        state.with(joinedRoomsState)
-}
+            state.with(JoinedRoomsState(
+                    (state.joinedRoomsState!!.rooms + joinedRoom),
+                    (state.joinedRoomsState.unreadCounts + joinedRoomUnreadCount)
+            ))
+        }
