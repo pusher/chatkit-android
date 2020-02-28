96b595d2b0204a6265d8656a97d095b06740c982
Danielle Vass <danielle.vass@pusher.com>
2020-02-28 12:46:37 +0000

    Put dateFormat val back

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.TimeZone
 
+internal val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
+
 internal object DateUtil {
     internal fun parseApiDateToEpoch(input: String): Long {
         val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
