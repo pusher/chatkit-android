e9509fafcd109d9e4fc0d5146075e454548731dd
Danielle Vass <danielle.vass@pusher.com>
2020-03-03 17:15:29 +0000

    Fix parsing the datetime correctly

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
@@ -2,14 +2,12 @@ package com.pusher.chatkit.util
 
 import java.text.SimpleDateFormat
 import java.util.Locale
-import java.util.TimeZone
 
 internal val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
 
 internal object DateUtil {
     internal fun parseApiDateToEpoch(input: String): Long {
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
-        sdf.timeZone = TimeZone.getTimeZone("GMT")
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.ENGLISH)
         return sdf.parse(input).time
     }
 }
