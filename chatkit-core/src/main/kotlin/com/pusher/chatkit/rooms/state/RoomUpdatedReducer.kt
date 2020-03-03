308840224e6ec89e319142770ead760b81a2640b
Danielle Vass <danielle.vass@pusher.com>
2020-03-03 17:36:15 +0000

    Remove double indentations in the reducers

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/state/RoomUpdatedReducer.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/rooms/state/RoomUpdatedReducer.kt
@@ -6,13 +6,13 @@ import org.reduxkotlin.reducerForActionType
 
 internal val roomUpdatedReducer =
     reducerForActionType<ChatkitState, RoomUpdated> { state, action ->
-        checkNotNull(state.joinedRoomsState)
+            checkNotNull(state.joinedRoomsState)
 
-        state.with(
+            state.with(
                 joinedRoomsState = JoinedRoomsState(
-                        state.joinedRoomsState.rooms.plus(action.room.id to action.room),
-                        state.joinedRoomsState.unreadCounts
+                    state.joinedRoomsState.rooms.plus(action.room.id to action.room),
+                    state.joinedRoomsState.unreadCounts
                 ),
                 auxiliaryState = state.auxiliaryState.with(action)
-        )
-}
+            )
+    }
