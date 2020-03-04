c217d686fea807e612f2697a9ad11c1488acbc89
Danielle Vass <danielle.vass@pusher.com>
2020-03-04 14:51:33 +0000

    Remove unnecessary constructor for DateApiTypeMapper

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/util/DateUtil.kt
@@ -5,7 +5,7 @@ import java.util.Locale
 
 internal val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX", Locale.ENGLISH)
 
-internal class DateApiTypeMapper() {
+internal class DateApiTypeMapper {
     fun mapToEpochTime(input: String): Long {
         return dateFormat.parse(input).time
     }
