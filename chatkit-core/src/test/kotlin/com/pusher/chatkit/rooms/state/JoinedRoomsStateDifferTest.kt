ff53ecb13820e658319a4d53e6828f6ace566d50
Danielle Vass <danielle.vass@pusher.com>
2020-03-05 11:18:31 +0000

    Add more tests for the JoinedRoomsStateDiffer

--- chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/state/JoinedRoomsStateDifferTest.kt
+++ chatkit-core/src/test/kotlin/com/pusher/chatkit/rooms/state/JoinedRoomsStateDifferTest.kt
@@ -3,6 +3,8 @@ package com.pusher.chatkit.rooms.state
 import assertk.assertThat
 import assertk.assertions.containsExactly
 import com.pusher.chatkit.state.JoinedRoom
+import com.pusher.chatkit.state.LeftRoom
+import com.pusher.chatkit.state.RoomUpdated
 import com.pusher.chatkit.state.State
 import io.mockk.every
 import io.mockk.mockk
@@ -18,18 +20,54 @@ class JoinedRoomsStateDifferTest : Spek({
 
         every { state().joinedRoomsState } returns JoinedRoomsState(
             rooms = mapOf(roomOneId to roomOne),
-            unreadCounts =  mapOf(roomOneId to 1))
+            unreadCounts = mapOf(roomOneId to 1))
 
-        describe("when room is joined"){
+        describe("when room is joined") {
             val actions = differ.toActions(
                 newRooms = listOf(roomOne, roomTwo),
                 newUnreadCounts = mapOf(roomOneId to 1, roomTwoId to 2))
 
-            it("then the result contains JoinedRoom action"){
+            it("then the result contains JoinedRoom action") {
                 assertThat(actions).containsExactly(JoinedRoom(room = roomTwo, unreadCount = 2))
             }
         }
     }
 
+    describe("given two rooms") {
+        val state = mockk<GetState<State>>(relaxed = true)
+        val differ = JoinedRoomsStateDiffer(state)
 
+        every { state().joinedRoomsState } returns JoinedRoomsState(
+            rooms = mapOf(roomOneId to roomOne, roomTwoId to roomTwo),
+            unreadCounts = mapOf(roomOneId to 1, roomTwoId to 2))
+
+        describe("when a room is left") {
+            val actions = differ.toActions(
+                newRooms = listOf(roomOne),
+                newUnreadCounts = mapOf(roomOneId to 1))
+
+            it("then the result contains RoomUpdated action") {
+                assertThat(actions).containsExactly(LeftRoom(roomId = roomTwoId))
+            }
+        }
+    }
+
+    describe("given two rooms") {
+        val state = mockk<GetState<State>>(relaxed = true)
+        val differ = JoinedRoomsStateDiffer(state)
+
+        every { state().joinedRoomsState } returns JoinedRoomsState(
+            rooms = mapOf(roomOneId to roomOne, roomTwoId to roomTwo),
+            unreadCounts = mapOf(roomOneId to 1, roomTwoId to 2))
+
+        describe("when a room is changed") {
+            val actions = differ.toActions(
+                newRooms = listOf(roomOneUpdated, roomTwo),
+                newUnreadCounts = mapOf(roomOneId to 1, roomTwoId to 2))
+
+            it("then the result contains RoomUpdated action") {
+                assertThat(actions).containsExactly(RoomUpdated(room = roomOneUpdated))
+            }
+        }
+    }
 })
