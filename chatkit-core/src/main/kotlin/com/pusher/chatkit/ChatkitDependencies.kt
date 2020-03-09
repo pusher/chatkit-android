ca3c3e4876321232cac09f2b6dca9769443a8871
Marek Urbaniak <8502071+marekoid@users.noreply.github.com>
2020-03-09 10:42:30 +0000

    Add docs for ChatkitDependencies

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitDependencies.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/ChatkitDependencies.kt
@@ -7,6 +7,17 @@ import com.pusher.platform.logger.Logger
 import com.pusher.platform.tokenProvider.TokenProvider
 import java.io.File
 
+/**
+ * Dependencies used to configure the Chatkit SDK, primarily to set your
+ * [TokenProvider] and optionally set other dependencies like your [Logger].
+ *
+ * @param tokenProvider for providing your JWT auth tokens, depending on your backend you may
+ * consider using the exiting [ChatkitTokenProvider] implementation
+ * @param logger pass if you want the SDK to log messages
+ * @param mediaTypeResolver pass if you upload files for message attachments so that
+ * HTTP Content-Type is set
+ * @param sdkInfo consider passing if you are building a specialised JVM-based SDK
+ */
 open class ChatkitDependencies(
     val tokenProvider: TokenProvider,
     override val logger: Logger = EmptyLogger(),
@@ -19,6 +30,9 @@ open class ChatkitDependencies(
     )
 ) : PlatformDependencies
 
+/**
+ * No-operation implementation. Extend if you just want to log messages only at specific levels.
+ */
 open class EmptyLogger : Logger {
     override fun debug(message: String, error: Error?) { /* nop */ }
     override fun error(message: String, error: Error?) { /* nop */ }
@@ -27,8 +41,11 @@ open class EmptyLogger : Logger {
     override fun warn(message: String, error: Error?) { /* nop */ }
 }
 
+/**
+ * Implementation that resolves file media types as unknown (`null`).
+ */
 class NullMediaTypeResolver : MediaTypeResolver {
     override fun fileMediaType(file: File): String? = null
 }
 
-const val SDK_VERSION = "2.0.0-alpha1" // should match VERSION_NAME in parent's gradle.properties
+internal const val SDK_VERSION = "2.0.0-alpha1" // should match VERSION_NAME in parent's gradle.properties
