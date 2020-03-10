c244f265fb5c0c5784a89d98dd15bd60cf48ee42
Marek Urbaniak <8502071+marekoid@users.noreply.github.com>
2020-03-10 14:26:03 +0000

    Create our StoreSubscriber
    Thanks to it a single dependency will be needed for the
    classes that subscribe to the store as observers will be notified
    with the current state object (as opposed to having to get
    a separate GetState dependency to retrieve the current state while
    being just notified that something has changed, i.e. () -> Unit).
    
    Additionally StoreSubscription has a clearly named unsubscribe
    function, as opposed just to subscribe fun return type of () -> Unit.
    
    Also with having a more concrete type there's no need for named
    qualifier which was added for clarity and clash avoidance on:
    (() -> Unit) -> (() -> Unit).

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/Store.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/Store.kt
@@ -6,14 +6,13 @@ import com.pusher.chatkit.rooms.state.leftRoomReducer
 import com.pusher.chatkit.rooms.state.roomDeletedReducer
 import com.pusher.chatkit.rooms.state.roomUpdatedReducer
 import org.koin.core.module.Module
-import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.reduxkotlin.combineReducers
 import org.reduxkotlin.createStore
 
 internal val createStoreModule: () -> Module = {
     module {
-        single(named(store::subscribe.name)) { store.subscribe }
+        single { StoreSubscriber(store) }
         single { store.getState }
 
         single { Dispatcher(store) }
@@ -21,6 +20,28 @@ internal val createStoreModule: () -> Module = {
 }
 
 internal typealias Store = org.reduxkotlin.Store<ChatkitState>
+
+internal typealias StoreObserver = (ChatkitState) -> Unit
+
+internal class StoreSubscriber(private val store: Store) : (StoreObserver) -> StoreSubscription {
+
+    override fun invoke(observer: StoreObserver) =
+        StoreSubscription(
+            store.subscribe {
+                observer(store.state)
+            }
+        )
+
+}
+
+internal class StoreSubscription(unsubscribe: () -> Unit) {
+
+    private val _unsubscribe = unsubscribe
+
+    fun unsubscribe() {
+        _unsubscribe()
+    }
+}
 
 internal class Dispatcher(private val store: Store) : (Action) -> Unit {
 
