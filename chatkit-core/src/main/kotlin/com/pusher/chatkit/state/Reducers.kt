d4e134adefe040d560154badaf8291fb5f1ab46d
Michael Pye <mail@mdpye.co.uk>
2020-03-17 10:32:24 +0000

    Introduce stateReducer and use to define chatStateReducer

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/state/Reducers.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/state/Reducers.kt
@@ -4,11 +4,20 @@ import org.reduxkotlin.ReducerForActionType
 import org.reduxkotlin.reducerForActionType
 
 /**
+ * Create a reducer for the whole State
+ */
+internal inline fun <reified TAction> stateReducer(
+    crossinline reducer: ReducerForActionType<State, TAction>
+) = reducerForActionType<State, TAction> { state, action ->
+    reducer(state, action)
+}
+
+/**
  * Create a slice reducer for ChatState branch
  */
 internal inline fun <reified TAction> chatStateReducer(
     crossinline reducer: ReducerForActionType<ChatState, TAction>
-) = reducerForActionType<State, TAction> { state, action ->
+) = stateReducer<TAction> { state, action ->
     state.with(
         chatState = reducer(state.chatState, action)
     )
