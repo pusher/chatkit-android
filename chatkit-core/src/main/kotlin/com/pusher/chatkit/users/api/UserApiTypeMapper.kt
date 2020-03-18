ef656ae26d5e61552941a30b7b4a82953be86924
Danielle Vass <danielle.vass@pusher.com>
2020-03-18 11:40:23 +0000

    Return element in UserApiTypeMapper directly

--- chatkit-core/src/main/kotlin/com/pusher/chatkit/users/api/UserApiTypeMapper.kt
+++ chatkit-core/src/main/kotlin/com/pusher/chatkit/users/api/UserApiTypeMapper.kt
@@ -4,8 +4,8 @@ import com.pusher.chatkit.users.state.UserInternalType
 import com.pusher.chatkit.util.DateApiTypeMapper
 
 internal class UserApiTypeMapper(private val dateApiTypeMapper: DateApiTypeMapper) {
-    fun toUserInternalType(user: UserApiType): UserInternalType {
-        return UserInternalType(
+    fun toUserInternalType(user: UserApiType): UserInternalType =
+        UserInternalType(
             id = user.id,
             createdAt = dateApiTypeMapper.mapToEpochTime(user.createdAt),
             updatedAt = dateApiTypeMapper.mapToEpochTime(user.updatedAt),
@@ -13,5 +13,4 @@ internal class UserApiTypeMapper(private val dateApiTypeMapper: DateApiTypeMappe
             avatarUrl = user.avatarUrl,
             customData = user.customData
         )
-    }
 }
